12cb005a1e0f75b54dd875f1eff3ae9501eefbb8
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2022-11-10 19:39:30 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -1,22 +1,22 @@
 /* Global Phaser */
 
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
-  physics: { 
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
     default: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
   //set background color
-backgroundColor: 0x5f6e7a,
+  backgroundColor: 0x5f6e7a,
   //Places background in the middle
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-}
-const game = new Phaser.Game(config)
-console.log(game)
+};
+const game = new Phaser.Game(config);
+console.log(game);
