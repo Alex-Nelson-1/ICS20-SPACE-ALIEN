3e64acb4627a93f0fe47aecd4d30517355980af0
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-11-21 18:42:39 +0000

    Got rid of the y7

--- js/game.js
+++ js/game.js
@@ -1,22 +1,35 @@
 /* Global Phaser */
+//
+//Created by: Alex Nelson
+//Created on: Nov 2022
+//This is the phaser 3 configuration file
+import SplashScene from "./splashScene.js"
+
+const splashScene = new SplashScene()
 
 const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  physics: {
+type: Phaser.AUTO,
+width: 1920,
+height: 1080,
+  physics: { 
     default: "arcade",
     arcade: {
-      debug: true,
-    },
+      debug: true
+    }
   },
   //set background color
-  backgroundColor: 0x5f6e7a,
+backgroundColor: 0x5f6e7a,
   //Places background in the middle
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-};
-const game = new Phaser.Game(config);
-console.log(game);
+}
+const game = new Phaser.Game(config)
+//console.log(game)
+
+//load scenes
+//Note: Any 'key' cannot be reused
+game.scene.add('splashScene', splashScene)
+//Start scene
+game.scene.start('splashScene')
---
+++ js/splashScene.js
@@ -0,0 +1,34 @@
+/* Global Phaser */
+//
+//Created by: Alex Nelson
+//Created on: Nov 2022
+//This is the Splash Scenes file
+
+/**
+* This class is the Splash scene
+*/
+class SplashScene extends Phaser.Scene{
+/** 
+* Method for constructor
+*/
+  constructor() {
+    super({key: 'splashScene'})
+  }
+  /**
+  * @param {object} data -
+  */
+  init(data){
+    this.cameras.main.setBackgroundColor("ffffff")
+  }
+  preload(){
+    console.log("Splash Scene")
+  }
+  create(data) {
+    //pass
+  }
+  update(time, delta){
+    //pass
+  }
+}
+
+export default SplashScene
