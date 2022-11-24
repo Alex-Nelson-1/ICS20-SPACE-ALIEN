ae3534b655edd1ff74bc8a40b282963f87e9d0ff
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2022-11-24 02:08:29 +0000

    Prettified Code!

--- js/splashScene.js
+++ js/splashScene.js
@@ -5,33 +5,37 @@
 //This is the Splash Scenes file
 
 /**
-* This class is the Splash scene
-*/
-class SplashScene extends Phaser.Scene{
-/** 
-* Method for constructor
-*/
+ * This class is the Splash scene
+ */
+class SplashScene extends Phaser.Scene {
+  /**
+   * Method for constructor
+   */
   constructor() {
-    super({key: 'splashScene'})
+    super({ key: "splashScene" });
   }
 
-  init(data){
-    this.cameras.main.setBackgroundColor("ffffff")
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
   }
-  preload(){
-    console.log("Splash Scene")
-    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
+  preload() {
+    console.log("Splash Scene");
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png");
   }
   create(data) {
-    this.splashSceneBackgroundImage = this.add.sprite(0, 0, "splashSceneBackground")
-    this.splashSceneBackgroundImage.x = 1920 / 2
-    this.splashSceneBackgroundImage.y = 1080 / 2
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    );
+    this.splashSceneBackgroundImage.x = 1920 / 2;
+    this.splashSceneBackgroundImage.y = 1080 / 2;
   }
-  update(time, delta){
+  update(time, delta) {
     if (time > 3000) {
-    this.scene.switch("titleScene")
+      this.scene.switch("titleScene");
     }
   }
 }
 
-export default SplashScene
+export default SplashScene;
