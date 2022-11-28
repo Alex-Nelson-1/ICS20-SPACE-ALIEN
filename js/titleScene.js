7eaefeeed730c5dcc451d1a201bb52e6d64d9402
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2022-11-28 22:43:21 +0000

    Prettified Code!

--- js/titleScene.js
+++ js/titleScene.js
@@ -12,43 +12,45 @@ class TitleScene extends Phaser.Scene {
    * Method for constructor
    */
   constructor() {
-    super({ key: "titleScene" })
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
+    super({ key: "titleScene" });
+    this.titleSceneBackgroundImage = null;
+    this.titleSceneText = null;
     this.titleSceneTextStyle = {
       font: "200px Times",
       fill: "#fde4b9",
       allign: "center",
-    }
+    };
   }
 
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("ffffff");
   }
 
   //loads title scene and displays "Title Scene" in the console
   preload() {
-    console.log("Title Scene")
+    console.log("Title Scene");
     this.load.image(
       "titleSceneBackground",
       "./assets/aliens_screen_image2.jpg"
-    )
+    );
   }
 
   //Displays the loaded image
   create(data) {
     this.titleSceneBackgroundImage = this.add
       .sprite(0, 0, "titleSceneBackground")
-      .setScale(2.75)
-    this.titleSceneBackgroundImage.x = 1920 / 2
-    this.titleSceneBackgroundImage.y = 1080 / 2
+      .setScale(2.75);
+    this.titleSceneBackgroundImage.x = 1920 / 2;
+    this.titleSceneBackgroundImage.y = 1080 / 2;
 
-    this.titleSceneText = this.add.text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle).setOrigin(0.5)
+    this.titleSceneText = this.add
+      .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+      .setOrigin(0.5);
   }
-  
+
   update(time, delta) {
     //pass
   }
 }
 
-export default TitleScene
+export default TitleScene;
