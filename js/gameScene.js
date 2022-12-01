510a6c2e70986ab504ccedf8647b46c64c121d33
Alex-Nelson-1 <Alex-Nelson-1@users.noreply.github.com>
2022-12-01 18:58:32 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -12,28 +12,28 @@ class GameScene extends Phaser.Scene {
    * Method for constructor
    */
   constructor() {
-    super({ key: "gameScene" })
+    super({ key: "gameScene" });
   }
 
   init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("ffffff");
   }
   //Displays "Game Scene" in the console
   preload() {
-    console.log("Game Scene")
+    console.log("Game Scene");
 
-    this.load.image("starBackground", "./assets/starBackground.png")
-    this.load.image("spaceShip", "./assets/spaceShip.png")
+    this.load.image("starBackground", "./assets/starBackground.png");
+    this.load.image("spaceShip", "./assets/spaceShip.png");
   }
   create(data) {
-    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
-    this.background.setOrigin(0, 0)
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
+    this.background.setOrigin(0, 0);
 
-    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "spaceShip")
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "spaceShip");
   }
   update(time, delta) {
     //pass
   }
 }
 
-export default GameScene
+export default GameScene;
